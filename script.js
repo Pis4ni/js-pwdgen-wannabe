dca99f17c89c233f54152479b171f3f1eea5477f
Pis4ni <pis4wrk@gmail.com>
2023-07-18 15:09:34 +0200

    add: script

---
+++ script.js
@@ -0,0 +1,15 @@
+const username = prompt("inserisci il tuo nome utente");
+const userSurname = prompt('inserisci il tuo cognome');
+const favColor = prompt('inserici il tuo colore preferito');
+
+const description = `questo è la tua nuova password, è stata creata concatenando il tuo nome, il tuo cognome e il tuo colore preferito! pensa te che figata assurda`
+
+const generated = username + userSurname + favColor;
+
+
+const welcomeMessage = `
+<h2>Benvenuto ${generated}</h2>
+<P>${description}</p>;
+`
+
+document.getElementById('display').innerHTML = welcomeMessage;
