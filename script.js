b9e2507024fffe31ec59273a6830594d169d26c7
Pis4ni <pis4wrk@gmail.com>
2023-07-18 17:41:22 +0200

    fix:  backtick usage

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const username = prompt("inserisci il tuo nome utente");
 const userSurname = prompt('inserisci il tuo cognome');
 const favColor = prompt('inserici il tuo colore preferito');
 
-const description = `questo è la tua nuova password, è stata creata concatenando il tuo nome, il tuo cognome e il tuo colore preferito! pensa te che figata assurda`
+const description = `questo è la tua nuova password, 
+è stata creata concatenando il tuo nome, il tuo cognome 
+e il tuo colore preferito! pensa te che figata assurda!`
 
 const generated = username + userSurname + favColor;
 
